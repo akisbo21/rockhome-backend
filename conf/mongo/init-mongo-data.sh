48144a9c1565c49c074835debbe1d600b82a890c
dagiur <gerendasbalint@gmail.com>
2024-04-03 13:56:49 +0200

    mongo init fix

--- conf/mongo/init-mongo-data.sh
+++ conf/mongo/init-mongo-data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mongoimport --username mongouser --password mongopass --host rock-mongo0 --port 27017 --db test --mode upsert --upsertFields external_id --collection rockhome --type json --file /conf/mongo/init-mongo-data.json --jsonArray
+mongoimport --username mongouser --password mongopass --authenticationDatabase=test --host rock-mongo0 --port 27017 --db test --mode upsert --upsertFields external_id --collection rockhome --type json --file /conf/mongo/init-mongo-data.json --jsonArray
